eb10f204930ad5d7083b9e9ebe80ffd2415fc128
Sadly4343 <loganhartshorn@gmail.com>
2024-11-19 17:33:53 -0700

    co

--- db_products.py
+++ db_products.py
@@ -1,5 +1,6 @@
 import boto3
 from flask import jsonify
+from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb = boto3.resource('dynamodb')
 products_table = dynamodb.Table('products')
@@ -10,7 +11,8 @@ def show_menu():
     print("2. Delete Item")
     print("3. Query Item")
     print("4. Display Table")
-    print("5. Quit")
+    print("5. Update Item")
+    print("6. Quit")
 
 
 def add_item():
@@ -30,7 +32,113 @@ def add_item():
             'product_price': product_price
         }
     )
-def delete_item():()
+def delete_item():
+    product_category = input("What is the product category? ")
+    product_id = input("What is the product Id? ")
+        
+    products_table.delete_item(
+        Key={
+            'category': product_category,
+            'product_id': product_id
+            }
+        )
+def query_item():
+    print("Select your Query Choice")
+    print("1. Query by Key")
+    print("2. Query by Attribute")
+    print("3. Quit")
+    choice = input("Choose your query ")
+    choiceint = int(choice)
+
+    if choiceint == 1:
+        keyvalue = input("Choose Category or Id").lower
+        if keyvalue == 'category':
+            category = input("Choose the category ")
+            response = products_table.query(
+            KeyConditionExpression=Key('category').eq(category))
+            
+            items = response['Items']
+            print(items)
+
+    elif choiceint == 2:
+        print("Select a value to query")
+        print("1. Query by name")
+        print("2. Query by product_id")
+        attrvalue = input("Choose a value to query")
+        attrvalueint = int(attrvalue)
+        if attrvalueint == 1:
+            name = input("What is your product name")
+            response = products_table.scan(
+                FilterExpression='product_name = :value',
+                ExpressionAttributeValues={
+                    ':value': name
+                }
+            )
+            item = response['Items']
+            print(item)
+        if attrvalueint == 2:
+            id = input("What is your product id")
+            response = products_table.scan(
+                FilterExpression='product_id = :value',
+                ExpressionAttributeValues={
+                    ':value': id
+                }
+            )
+            item = response['Items']
+            print(item)
+
+    
+def update_item():
+    category = input("What is the category").lower()
+    id = input("What is the product Id")
+    print("Choose what to update")
+    print("1. Description")
+    print("2. Name")
+    print("3. Price")
+    update = input("What would you like to update? ")
+    updateint = int(update)
+    if updateint == 1:
+        description = input("What is your description? ")
+        products_table.update_item(
+            Key={
+            'category': category,
+            'product_id': id
+            },
+            UpdateExpression='SET product_description = :val1',
+            ExpressionAttributeValues={
+                ':val1': description
+            }
+        )
+
+    elif updateint == 2:
+        name = input("What is your new name? ")
+        products_table.update_item(
+            Key={
+            'category': category,
+            'product_id': id
+            },
+            UpdateExpression='SET product_name = :val1',
+            ExpressionAttributeValues={
+                ':val1': name
+            }
+        )
+    elif updateint == 3:
+        price = input("What is your new Price? ")
+        printint = int(price)
+        products_table.update_item(
+            Key={
+            'category': category,
+            'product_id': id
+            },
+            UpdateExpression='SET product_price = :val1',
+            ExpressionAttributeValues={
+                ':val1': printint
+            }
+        )
+    else:
+        ()
+    
+
 
 def display_item():
     response = products_table.scan()
@@ -39,6 +147,30 @@ def display_item():
 
     for item in items:
         print(item)
+def menuselect(choice):
+    if choice == '1':
+        add_item()
+    elif choice == '2':
+        delete_item()
+    elif choice == '3':
+        query_item()
+    elif choice == '4':
+        display_item()
+    elif choice == '5':
+        update_item()
+    elif choice == '6':
+        print("Thanks for using this program")
+        return False
+    else:
+        print("Not correct choice")
+    return True
+
+def main():
+    menu = True
+    while menu:
+        show_menu()
+        choice = input("Enter Your Selection ")
+        menu = menuselect(choice)
 
 if __name__ == "__main__":
-    display_item()
+    main()
--- db_users.py
+++ db_users.py
@@ -1,8 +1,10 @@
 import boto3
 from flask import jsonify
+from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb = boto3.resource('dynamodb')
 user_table = dynamodb.Table('users')
+
 
 def show_menu():
     print("Select your Menu Choice")
@@ -10,7 +12,8 @@ def show_menu():
     print("2. Delete Item")
     print("3. Query Item")
     print("4. Display Table")
-    print("5. Quit")
+    print("5. Modify Item")
+    print("6. Quit")
 
 
 def add_item():
@@ -25,7 +28,90 @@ def add_item():
             'password': password
         }
     )
+def delete_item():
+    username = input("What is the username? ")
+        
+    user_table.delete_item(
+        Key={
+            'username': username
+            }
+        )
+def query_item():
+    print("Select your Query Choice")
+    print("1. Query by Key")
+    print("2. Query by Attribute")
+    print("3. Quit")
+    choice = input("Choose your query ")
+    choiceint = int(choice)
 
+    if choiceint == 1:
+            username = input("Choose the username ")
+            response = user_table.query(
+            KeyConditionExpression=Key('username').eq(username))
+            
+            items = response['Items']
+            print(items)
+
+    elif choiceint == 2:
+        print("Select a value to query")
+        print("1. Query by name")
+        print("2. Query by password")
+        attrvalue = input("Choose a value to query")
+        attrvalueint = int(attrvalue)
+        if attrvalueint == 1:
+            email = input("What is your email? ")
+            response = user_table.scan(
+                FilterExpression='email = :value',
+                ExpressionAttributeValues={
+                    ':value': email
+                }
+            )
+            item = response['Items']
+            print(item)
+        if attrvalueint == 2:
+            password = input("What is your password? ")
+            response = user_table.scan(
+                FilterExpression='password = :value',
+                ExpressionAttributeValues={
+                    ':value': password
+                }
+            )
+            item = response['Items']
+            print(item)
+
+def update_item():
+    user = input("What is the username").lower()
+    print("Choose what to update")
+    print("1. Email")
+    print("2. Password")
+    update = input("What would you like to update? ")
+    updateint = int(update)
+    if updateint == 1:
+        email = input("What is your new email? ")
+        user_table.update_item(
+            Key={
+            'username': user
+            },
+            UpdateExpression='SET email = :val1',
+            ExpressionAttributeValues={
+                ':val1': email
+            }
+        )
+
+    elif updateint == 2:
+        password = input("What is your new password? ")
+        user_table.update_item(
+            Key={
+            'username': user
+            },
+            UpdateExpression='SET password = :val1',
+            ExpressionAttributeValues={
+                ':val1': password
+            }
+        )
+    else:
+        ()
+    
 def display_item():
     response = user_table.scan()
 
@@ -34,5 +120,30 @@ def display_item():
     for item in items:
         print(item)
 
+def menuselect(choice):
+    if choice == '1':
+        add_item()
+    elif choice == '2':
+        delete_item()
+    elif choice == '3':
+        query_item()
+    elif choice == '4':
+        display_item()
+    elif choice == '5':
+        update_item()
+    elif choice == '6':
+        print("Thanks for using this program")
+        return False
+    else:
+        print("Not correct choice")
+    return True
+
+def main():
+    menu = True
+    while menu:
+        show_menu()
+        choice = input("Enter Your Selection ")
+        menu = menuselect(choice)
+
 if __name__ == "__main__":
-    display_item()
+    main()
